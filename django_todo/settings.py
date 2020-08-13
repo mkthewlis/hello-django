413ec07e53ef9e4445157abdcba9999d85c5a006
mkthewlis <mkthewlis@gmail.com>
2020-08-13 18:35:56 +0000

    corrected missing development variable to enable heroku deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-development = os.environ.get
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
