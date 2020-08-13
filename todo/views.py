7a03148495de9af017c123d22685a57804e17c63
mkthewlis <mkthewlis@gmail.com>
2020-08-13 07:37:12 +0000

    feat: added C and R functionality of CRUD

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import ItemForm
 # Create your views here.
 
-def say_hello(request):
-    return HttpResponse("Hello!")
+
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    # create instance of item form and context that contains empty form
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
