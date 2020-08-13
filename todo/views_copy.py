cb335c4a16b325cc628b58610c36725b0db4126b
mkthewlis <mkthewlis@gmail.com>
2020-08-13 08:11:58 +0000

    feat: added functionality to edit and delete tasks too

---
+++ todo/views_copy.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import ItemForm
+# Create your views here.
+
+
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        # django automatically compares the data submitted in the POST
+        # request by the is_valid method
+        if form.is_valid():
+            form.save()
+        return redirect('get_todo_list')
+    # create instance of item form and context that contains empty form
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    return render(request, 'todo/edit_item.html')
